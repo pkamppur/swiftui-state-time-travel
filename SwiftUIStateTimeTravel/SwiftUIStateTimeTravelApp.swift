ba6ea4decd72aba8ee6779569c0c438f35ce126a
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-10 19:41:02 +0200

    Initial Commit

---
+++ SwiftUIStateTimeTravel/SwiftUIStateTimeTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStateTimeTravelApp.swift
+//  SwiftUIStateTimeTravel
+//
+//  Created by Petteri Kamppuri on 10.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStateTimeTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
