773a691ca3e46cdc4866fca19ae5d560eea0847c
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-13 19:53:03 +0200

    add BoxFooter

---
+++ SwiftUIStateTimeTravel/BoxFooter.swift
@@ -0,0 +1,23 @@
+//
+//  BoxFooter.swift
+//  SwiftUIStateTimeTravel
+//
+//  Created by Petteri Kamppuri on 10.11.2020.
+//
+
+import SwiftUI
+
+struct BoxFooter: View {
+    @Binding var boxes: [Box]
+    
+    var body: some View {
+        VStack {
+            AddButton(boxes: $boxes)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+        .background(
+            Text("Box count: \(boxes.count)")
+                .padding()
+        )
+    }
+}
