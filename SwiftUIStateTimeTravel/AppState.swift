05eff34ac877ed21b0106656f099a2511ccccedf
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-13 19:51:09 +0200

    add AppState

---
+++ SwiftUIStateTimeTravel/AppState.swift
@@ -0,0 +1,38 @@
+//
+//  AppState.swift
+//  SwiftUIStateTimeTravel
+//
+//  Created by Petteri Kamppuri on 10.11.2020.
+//
+
+import UIKit
+import SwiftUI
+
+struct AppState: Equatable {
+    var boxes: [Box]
+}
+
+struct Box: Equatable {
+    let name: String
+    let color: UIColor
+}
+
+
+
+
+
+extension Box {
+    var backgroundColor: Color {
+        Color(color)
+    }
+}
+
+func sampleAppState() -> AppState {
+    let boxes = [
+        Box(name: "Red", color: UIColor.red),
+        Box(name: "Orange", color: UIColor.orange),
+        Box(name: "Purple", color: UIColor.purple),
+        Box(name: "Blue", color: UIColor.blue),
+    ]
+    return AppState(boxes: boxes)
+}
