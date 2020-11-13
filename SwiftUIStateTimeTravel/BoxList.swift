c4b5984c4648026cb2e818d6c12483d6e3648733
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-13 20:08:18 +0200

    add BoxList

---
+++ SwiftUIStateTimeTravel/BoxList.swift
@@ -0,0 +1,42 @@
+//
+//  BoxList.swift
+//  SwiftUIStateTimeTravel
+//
+//  Created by Petteri Kamppuri on 10.11.2020.
+//
+
+import SwiftUI
+
+struct BoxList: View {
+    @Binding var boxes: [Box]
+    
+    var body: some View {
+        VStack {
+            AddButton(boxes: $boxes)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+
+            List(boxes, id: \.name) {
+                BoxView(box: $0)
+            }
+        }
+    }
+}
+
+struct BoxView: View {
+    let box: Box
+    
+    var body: some View {
+        Text(box.name)
+            .padding()
+            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+            .background(
+                LinearGradient(
+                    gradient: Gradient(colors: [
+                        box.backgroundColor,
+                        box.backgroundColor.opacity(0.25)
+                    ]),
+                    startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(5)
+    }
+}
