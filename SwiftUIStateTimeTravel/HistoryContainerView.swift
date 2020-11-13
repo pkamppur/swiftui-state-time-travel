277f9d9a0b90866ed658206d8935d4e15944e5b0
Petteri Kamppuri <petteri.kamppuri@me.com>
2020-11-13 20:13:44 +0200

    record state history

--- SwiftUIStateTimeTravel/HistoryContainerView.swift
+++ SwiftUIStateTimeTravel/HistoryContainerView.swift
@@ -8,12 +8,30 @@
 import SwiftUI
 
 struct HistoryContainerView: View {
+    @State private var stateHistory = [AppState]()
+    @State private var stateIndex = -1
     @State private var state = sampleAppState()
 
     var body: some View {
         VStack(alignment: .leading) {
             BoxList(boxes: $state.boxes) // pass in only subset of global app data to minimize updates (I hope)
             BoxFooter(boxes: $state.boxes)
+        }
+        .onChange(of: state) { newState in
+            let isChange = stateHistory[stateIndex] != newState
+            if !isChange {
+                return
+            }
+
+            stateHistory.removeSubrange((stateIndex + 1)..<stateHistory.count)
+            stateHistory += [newState]
+            stateIndex += 1
+        }
+        .onAppear() {
+            if stateIndex == -1 {
+                stateIndex = 0
+                stateHistory = [state]
+            }
         }
     }
 }
